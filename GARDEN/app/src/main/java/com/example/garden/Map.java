a5deebb32547fade4268f76aa9ec9f9d04653676
DGKD <49873598+DGKD@users.noreply.github.com>
2019-11-27 20:11:05 +0800

    GARDEN
    function for scan qrcode and Multiple choice question

---
+++ GARDEN/app/src/main/java/com/example/garden/Map.java
@@ -0,0 +1,8 @@
+package com.example.garden;
+
+import android.os.Bundle;
+
+public class Map extends MainActivity {public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.map);
+}}
---
+++ GARDEN/app/src/main/java/com/example/garden/US.java
@@ -0,0 +1,9 @@
+package com.example.garden;
+
+import android.os.Bundle;
+
+public class US extends MainActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contantus);
+}}
